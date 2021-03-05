9164ecd5b658c6fc73efb93afa2bf009abaa0127
Ivannikov Roman <roman.y.ivannikov@gmail.com>
2021-03-05 07:00:02 +0700

    lesson 26 practice start

--- js/script.js
+++ js/script.js
@@ -388,4 +388,49 @@ window.addEventListener('DOMContentLoaded', function() {
     };
     calc(100);
 
+    //send ajax form
+    const sendForm = () => {
+        const errorMessage = 'Что-то пошло не так...',
+            loadMessage = 'Загрузка...',
+            successMessage = 'Спасибо! Мы скоро с вами свяжемся!';
+        
+        const form = document.getElementById('form1');
+
+        const statusMessage = document.createElement('div');
+        statusMessage.style.cssText = 'font-size: 2rem;';
+        
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+            form.appendChild(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.addEventListener('readystatechange', () => {
+                statusMessage.textContent = loadMessage;
+
+                if (request.readyState !== 4) {
+                    return;
+                }
+
+                if (request.status === 200) {
+                    statusMessage.textContent = successMessage;
+                } else {
+                    statusMessage.textContent = errorMessage;
+                }
+            });
+
+            request.open('POST', './server.php');
+            request.setRequestHeader('Content-Type', 'application/json');
+            const formData = new FormData(form);
+            let body = {};
+            
+            formData.forEach((val, key) => {
+                body[key] = val;
+            });
+
+            request.send(JSON.stringify(body));
+        });
+
+    };
+    sendForm();
+
 });
