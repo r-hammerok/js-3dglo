271273519c68eeb5df18049f5d0f909e29c08c09
Roman Ivannikov <roman.y.ivannikov@gmail.com>
2021-03-05 16:27:07 +0700

    lesson 26 normal begin

--- js/script.js
+++ js/script.js
@@ -394,10 +394,10 @@ window.addEventListener('DOMContentLoaded', function() {
             loadMessage = 'Загрузка...',
             successMessage = 'Спасибо! Мы скоро с вами свяжемся!';
         
-        const form = document.getElementById('form1');
+        const cssType1 = 'font-size: 2rem;',
+            cssType2 = 'font-size: 1.5rem; color: white;';
 
         const statusMessage = document.createElement('div');
-        statusMessage.style.cssText = 'font-size: 2rem;';
         
         const postData = (body, outputData, errorData) => {
             const request = new XMLHttpRequest();
@@ -416,8 +416,15 @@ window.addEventListener('DOMContentLoaded', function() {
             request.send(JSON.stringify(body));
         };
 
-        form.addEventListener('submit', (event) => {
+        document.body.addEventListener('submit', (event) => {
             event.preventDefault();
+
+            // Так как submit срабатывает только для форм, то нет смысла проверять target
+            // он всегда будет <form>
+            const targetId = event.target.id;
+            const form = document.getElementById(targetId);
+            statusMessage.style.cssText = targetId === 'form3' ? cssType2 : cssType1;
+            
             form.appendChild(statusMessage);
             statusMessage.textContent = loadMessage;
             const formData = new FormData(form);
@@ -430,14 +437,39 @@ window.addEventListener('DOMContentLoaded', function() {
             postData(body,
                 () => {
                     statusMessage.textContent = successMessage;
+                    form.querySelectorAll('input').forEach((item) => {
+                        item.value = '';
+                    });
                 },
                 (error) => {
                     statusMessage.textContent = errorMessage;
                     console.error(error);
                 }
             );
-
         });
+
+        // form.addEventListener('submit', (event) => {
+        //     event.preventDefault();
+        //     form.appendChild(statusMessage);
+        //     statusMessage.textContent = loadMessage;
+        //     const formData = new FormData(form);
+        //     let body = {};
+            
+        //     formData.forEach((val, key) => {
+        //         body[key] = val;
+        //     });
+
+        //     postData(body,
+        //         () => {
+        //             statusMessage.textContent = successMessage;
+        //         },
+        //         (error) => {
+        //             statusMessage.textContent = errorMessage;
+        //             console.error(error);
+        //         }
+        //     );
+
+        // });
 
     };
     sendForm();
